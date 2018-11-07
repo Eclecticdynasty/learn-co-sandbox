b0a9689d094d4f64dfafe0d85b5298747d05a56e
Siobhan Martin <bklyngal1994@gmail.com>
2018-11-07 17:41:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+let friendCount = 3;
+ 
+let message, messageColor;
+ 
+if (user && friendCount) {
+    message = `Hi ${user}! You have ${friendCount} ${
+        friendCount === 1 ? 'friend' : 'friends'
+    }!`;
+} else if (user) {
+    message = 'Link up with your friends to get the most out of Flatbook!';
+} else {
+    message = 'Please sign in.';
+}
+
+if (
+    message === 'Please sign in.' ||
+    message === 'Link up with your friends to get the most out of Flatbook!'
+) {
+    messageColor = 'red';
+} else if (friendCount >= 1 && friendCount <= 10) {
+    messageColor = 'blue';
+} else if (friendCount >= 11 && friendCount <= 50) {
+    messageColor = 'purple';
+} else {
+    messageColor = 'rainbow';
+}
+
